075479d3a4d0216d332505eb275f030bb4825789
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-06-12 13:54:59 +0530

    Update 2 mistakes

--- tcs_A-to-Z_dsa/01patterns/20p.cpp
+++ tcs_A-to-Z_dsa/01patterns/20p.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 
 void pattern(int n){
-    
+          
+  int spaces = 2*n - 2;
+
   for(int i=1;i<=2*n-1;i++){
     int stars = i;
-    if(i>n)stars = 2*(n-i);
+    if(i>n) stars = 2*n-i;
     
     // stars 
     for(int j=1;j<=stars;j++)
      cout<<"*";
 
     // spaces 
-    for(int j=1;j<=2*n-2;j++){
+
+    for(int j=1;j<=spaces;j++){
         cout<<" ";
     }
+    if(i<n)spaces=spaces-2;
+    else spaces=spaces+2;
     // stars 
     for(int j=1;j<=stars;j++)
      cout<<"*";
